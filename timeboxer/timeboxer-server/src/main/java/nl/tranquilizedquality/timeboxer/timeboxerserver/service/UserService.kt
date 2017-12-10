6cae23abe4a80fffc667a0a794b0a0dc0f490221
Eduardo Gomez <edugof@gmail.com>
2017-12-10 16:07:26 +0100

    Expression body ftw

--- timeboxer/timeboxer-server/src/main/java/nl/tranquilizedquality/timeboxer/timeboxerserver/service/UserService.kt
+++ timeboxer/timeboxer-server/src/main/java/nl/tranquilizedquality/timeboxer/timeboxerserver/service/UserService.kt
@@ -3,20 +3,10 @@ package nl.tranquilizedquality.timeboxer.timeboxerserver.service
 import nl.tranquilizedquality.timeboxer.timeboxerserver.domain.User
 import nl.tranquilizedquality.timeboxer.timeboxerserver.domain.repository.UserRepository
 import org.springframework.stereotype.Component
-import java.util.Optional
 
 @Component
 class UserService(private val userRepository: UserRepository) {
-
-    fun getUser(userId: Long) : Optional<User> {
-        return userRepository.findById(userId)
-    }
-
-    fun createUser(user: User) {
-        userRepository.save(user)
-    }
-
-    fun updateUser(user: User) {
-        userRepository.save(user)
-    }
+    fun getUser(userId: Long) = userRepository.findById(userId)
+    fun createUser(user: User) = userRepository.save(user)
+    fun updateUser(user: User) = userRepository.save(user)
 }
