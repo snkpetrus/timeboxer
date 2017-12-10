080076ca1061bdfe235af10a641a015ef5a1974b
Eduardo Gomez <edugof@gmail.com>
2017-12-10 15:47:30 +0100

    Add team name

--- timeboxer/timeboxer-server/src/main/java/nl/tranquilizedquality/timeboxer/timeboxerserver/domain/Team.kt
+++ timeboxer/timeboxer-server/src/main/java/nl/tranquilizedquality/timeboxer/timeboxerserver/domain/Team.kt
@@ -1,4 +1,5 @@
 package nl.tranquilizedquality.timeboxer.timeboxerserver.domain
 
 data class Team(val teamId: Long? = null,
+                val name: String? = null,
                 val users: List<User> = mutableListOf())
