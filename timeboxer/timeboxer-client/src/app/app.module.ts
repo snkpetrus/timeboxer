b0b5b223b322926046644a43424c4e3d21369add
zhuravski <alex.besteiro@gmail.com>
2017-12-10 16:06:42 +0100

    Creating user details component

--- timeboxer/timeboxer-client/src/app/app.module.ts
+++ timeboxer/timeboxer-client/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import{BrowserModule}from'@angular/platform-browser';
+import {NgModule }from '@angular/core';
+import {FormsModule}from '@angular/forms';
 
-
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+import {AppComponent}from './app.component';
+import { UsersComponent}from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
